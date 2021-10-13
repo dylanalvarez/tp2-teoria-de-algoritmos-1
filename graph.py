2b2953e46cb77f2c875bfe461ecd2f01cf99786d
Juan Pablo Rombolá <juampi.rombola@gmail.com>
2021-10-13 20:09:49 -0300

    Pair Programming w/Robin: add Graph class

---
+++ graph.py
@@ -0,0 +1,17 @@
+class Graph:
+  def __init__(self):
+    self._nodes = set()
+    self._graph = {}
+
+  def add_edge(self, u, v, w):
+    self._nodes = self._nodes | set(u)
+    self._nodes = self._nodes | set(v)
+    value = self._graph.get(u, {})
+    value[v] = w
+    self._graph[u] = value
+
+  def get_nodes(self):
+    return self._nodes
+
+  def get_edges(self):
+    return [(entry[0], edge[0], edge[1]) for entry in self._graph.items() for edge in entry[1].items()]
